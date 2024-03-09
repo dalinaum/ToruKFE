8e07dd5ce40ebb802e0772b0d6ea5f471dc3c059
Toru Choi <blueocean239@gmail.com>
2024-03-09 17:47:37 +0900

    feat: added new 2 method which can be cascaded

--- app/src/main/java/kr/toru/kotlinflowevent/domain/network/ApiResponse.kt
+++ app/src/main/java/kr/toru/kotlinflowevent/domain/network/ApiResponse.kt
@@ -4,3 +4,17 @@ sealed class ApiResponse<T> {
     data class Success<T>(val data: T): ApiResponse<T>()
     data class Failure<T>(val exception: Exception): ApiResponse<T>()
 }
+
+fun<T> ApiResponse<T>.onSuccess(action: (T) -> Unit): ApiResponse<T> {
+    if (this is ApiResponse.Success) {
+        action(data)
+    }
+    return this
+}
+
+fun<T> ApiResponse<T>.onFailure(action: (Exception) -> Unit): ApiResponse<T> {
+    if (this is ApiResponse.Failure) {
+        action(exception)
+    }
+    return this
+}
