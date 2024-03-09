e4fcf8356deddbd1000f8e78846b03bbbfe35ca4
Toru Choi <blueocean239@gmail.com>
2024-03-09 23:55:13 +0900

    feat: added event using flow

--- app/src/main/java/kr/toru/kotlinflowevent/presentation/ui/MainActivity.kt
+++ app/src/main/java/kr/toru/kotlinflowevent/presentation/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package kr.toru.kotlinflowevent.presentation.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -8,8 +9,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kr.toru.kotlinflowevent.R
+import kr.toru.kotlinflowevent.presentation.viewmodel.Event
 import kr.toru.kotlinflowevent.presentation.viewmodel.MainViewModel
 
 @AndroidEntryPoint
@@ -29,6 +33,26 @@ class MainActivity : AppCompatActivity() {
 
         lifecycleScope.launch {
             viewModel.start()
+            viewModel.eventFlow.collectLatest { event ->
+                onHandleEvent(event)
+            }
+
+        }
+    }
+
+    private fun onHandleEvent(event: Event) {
+        when(event) {
+            is Event.Loading -> {
+                Log.e("Toru", "OnHandleEvent: Loading")
+            }
+            is Event.LoadedList -> {
+                // do something
+                Log.e("Toru", "OnHandleEvent: Loaded with List")
+            }
+            is Event.EmptyList -> {
+                // do something
+                Log.e("Toru", "OnHandleEvent: Empty List")
+            }
         }
     }
 }
--- app/src/main/java/kr/toru/kotlinflowevent/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/kr/toru/kotlinflowevent/presentation/viewmodel/MainViewModel.kt
@@ -4,6 +4,9 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kr.toru.kotlinflowevent.domain.repository.FakeJsonRepository
@@ -12,13 +15,29 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val repository: FakeJsonRepository
-)   : ViewModel() {
+) : ViewModel() {
+
+    private var _eventFlow = MutableStateFlow<Event>(Event.Loading)
+    var eventFlow = _eventFlow.asStateFlow()
 
     suspend fun start() {
         viewModelScope.launch {
-            repository.getPost().collectLatest { post ->
+            repository.getPost().collect { post ->
                 Log.e("Toru", "length: ${post.size}")
+
+                val event = if(post.isNotEmpty()) {
+                    Event.LoadedList(post.map { it.title })
+                } else {
+                    Event.EmptyList
+                }
+                _eventFlow.emit(event)
             }
         }
     }
+}
+
+sealed class Event {
+    data object Loading: Event()
+    data class LoadedList(val list: List<String>): Event()
+    data object EmptyList: Event()
 }
